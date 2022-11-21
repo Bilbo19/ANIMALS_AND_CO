ae5623ca4a36bdd153801b4bcba05612299994df
arielle nganso <arielle.nganso@gmail.com>
2022-11-21 15:28:06 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :animals, except: [:edit, :update]
+  resources :booking, only: [:index, :edit, :update]
 end
