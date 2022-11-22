165346930ec1c26c2e30971332ed8f6c3073bb5b
arielle nganso <arielle.nganso@gmail.com>
2022-11-22 11:24:27 +0100

    add validates to model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,8 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :age, presence: true
+  validates :description, presence: true, length: { minimum: 50, maximum: 200 }
 end
