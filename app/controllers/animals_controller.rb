30725db41d5c2468200f45c5cffb2db721160e5c
arielle nganso <arielle.nganso@gmail.com>
2022-11-22 15:05:05 +0100

    animal-authorization

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,30 +2,34 @@ class AnimalsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @animals = Animal.all
+    @animals = policy_scope(Animal)
   end
 
   def show
     @animal = Animal.find(params[:animal_id])
+    authorize @animal
   end
 
   def new
     @animal = Animal.new
+    authorize @animal
   end
 
   def create
     @animal = Animal.new(animal_params)
     @animal.user = current_user
     if @animal.save
-     redirect_to animals_path
+      redirect_to animals_path
     else
       render :new
     end
+    authorize @animal
   end
 
   def destroy
     @animal.destroy
     redirect_to animals_path
+    authorize @animal
   end
 
   private
