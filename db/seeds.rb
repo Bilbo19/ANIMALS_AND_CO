d80ae6328f14d9e67aefb5267d6b6888737ae577
Moutaka Lamba <lmoutaka@yahoo.fr>
2022-11-25 11:14:23 +0100

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 require 'faker'
+require 'open-uri'
 
 Animal.destroy_all
 User.destroy_all
-
  user1 = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -14,7 +14,6 @@ User.destroy_all
   password_confirmation: '123456'
  )
  user1.save!
-
  user2 = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -24,7 +23,6 @@ User.destroy_all
   password_confirmation: '123456'
  )
  user2.save!
-
  user3 = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -34,7 +32,6 @@ User.destroy_all
   password_confirmation: '123456'
  )
  user3.save!
-
  user4 = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -44,7 +41,6 @@ User.destroy_all
   password_confirmation: '123456'
  )
  user4.save!
-
  user5 = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -54,87 +50,7 @@ User.destroy_all
   password_confirmation: '123456'
  )
  user5.save!
-
 puts 'Creating 15 fake animals...'
-
-<<<<<<< HEAD
-animal = Animal.new(
-  name: "Oscar",
-  description: "Blablablabla",
-  category: "mammals",
-  age: 12,
-  price: 43,
-  address: '5 Rue Crespin du Gast, 75011 Paris',
-  user_id: user1.id
-)
-animal.save!
-=======
-# 3.times do
-#   animal = Animal.new(
-#     name:    Faker::Creature::Animal.name,
-#     description:Faker::Quote.famous_last_words,
-#     category: "mammals",
-#     age: Faker::Number.within(range: 1..100),
-#     price: 43,
-#     address: '5 Rue Crespin du Gast, 75011 Paris',
-#     user_id: user1.id
-#   )
-#   animal.save!
-# end
->>>>>>> 97b7728c74015ec8d43159fb500b99f894677e96
-
-# 3.times do
-#   animal = Animal.new(
-#     name:    Faker::Creature::Animal.name,
-#     description:Faker::Quote.famous_last_words,
-#     category: "insects",
-#     age: Faker::Number.within(range: 1..100),
-#     price: 10,
-#     address: '5 Rue de Thorigny, 75003 Paris',
-#     user_id: user2.id
-#   )
-#   animal.save!
-# end
-
-# 3.times do
-#   animal = Animal.new(
-#     name:    Faker::Creature::Animal.name,
-#     description:Faker::Quote.famous_last_words,
-#     category: "pisces",
-#     age: Faker::Number.within(range: 1..100),
-#     price: 55,
-#     address: '10 Bd Montmartre, 75009 Paris',
-#     user_id: user3.id
-#   )
-#   animal.save!
-# end
-
-# 3.times do
-#   animal = Animal.new(
-#     name: Faker::Name.first_name,
-#     description: Faker::Quote.famous_last_words,
-#     category: "birds",
-#     age: Faker::Number.within(range: 1..100),
-#     price: 20,
-#     address: '8 Rue Elzevir, 75003 Paris',
-#     user_id: user4.id
-#   )
-#   animal.save!
-# end
-
-# 3.times do
-#   animal = Animal.new(
-#     name: Faker::Name.first_name,
-#     description: Faker::Quote.famous_last_words,
-#     category: "reptiles",
-#     age: Faker::Number.within(range: 1..100),
-#     price: 14,
-#     address: '60 Rue Réaumur, 75003 Paris',
-#     user_id: user5.id
-#   )
-#   animal.save!
-# end
-
 
 animal = Animal.new(
   name: "Oscar",
@@ -143,10 +59,11 @@ animal = Animal.new(
   age: 57,
   price: 130,
   address: '5 Rue Crespin du Gast, 75011 Paris',
-  user_id: user1.id,
-  photo_url: "https://www.wildlifearchives.com/wp-content/uploads/2018/01/whales-most-beautiful-picture-1516721809gkn48.jpg"
+  user: User.first
 )
-animal.save!
+  file = URI.open("https://www.wildlifearchives.com/wp-content/uploads/2018/01/whales-most-beautiful-picture-1516721809gkn48.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: 'Denis',
@@ -155,10 +72,11 @@ animal = Animal.new(
   age: 15,
   price: 45,
   address: '29 Rue Charcot, 75013 Paris',
-  user_id: user1.id,
-  photo_url: "https://secure.img1-cg.wfcdn.com/im/87624362/compr-r85/1539/153900257/standing-donkey-statue.jpg"
+  user: user1
 )
-animal.save!
+  file = URI.open("https://secure.img1-cg.wfcdn.com/im/87624362/compr-r85/1539/153900257/standing-donkey-statue.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Lucas",
@@ -167,10 +85,11 @@ animal = Animal.new(
   age: 14,
   price: 20,
   address: '263 Boulevard Voltaire, 75011 Paris',
-  user_id: user1.id,
-  photo_url: "https://i.pinimg.com/originals/04/f2/f8/04f2f89aa366ce677fa28fdfe0d6d7a2.jpg"
+  user: user2
 )
-animal.save!
+  file = URI.open("https://i.pinimg.com/originals/04/f2/f8/04f2f89aa366ce677fa28fdfe0d6d7a2.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Alena",
@@ -179,22 +98,24 @@ animal = Animal.new(
   age: 11,
   price: 33,
   address: '51 Rue Guy Moquet, 75017 Paris',
-  user_id: user1.id,
-  photo_url: "https://media.istockphoto.com/id/453082743/fr/photo/golden-eagle.jpg?s=612x612&w=0&k=20&c=Q75BuJdYYh2C79RolaLjSDAArBBwvoaoQH06xZaH0y0="
+  user: user3
 )
-animal.save!
+  file = URI.open("https://media.istockphoto.com/id/453082743/fr/photo/golden-eagle.jpg?s=612x612&w=0&k=20&c=Q75BuJdYYh2C79RolaLjSDAArBBwvoaoQH06xZaH0y0=")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Youssef",
   description: "The alligator is a reptile of the order of crocodiles and it is an aggressive animal",
-  category: "Reptilians",
+  category: "reptilians",
   age: 14,
   price: 20,
   address: '263 Boulevard Voltaire, 75011 Paris',
-  user_id: user1.id,
-  photo_url: "https://www.earthtouchnews.com/media/1281943/02042015-_MG_0501.jpg"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://www.earthtouchnews.com/media/1281943/02042015-_MG_0501.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Veronika",
@@ -203,10 +124,11 @@ animal = Animal.new(
   age: 26,
   price: 60,
   address: '14 Rue Chabanais, 75002 Paris',
-  user_id: user1.id,
-  photo_url: "https://preview.redd.it/52ialyqvcxg51.jpg?width=640&crop=smart&auto=webp&s=07d5c20f5f947484d8f6edfd030c5ad17ddfe5d4"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://preview.redd.it/52ialyqvcxg51.jpg?width=640&crop=smart&auto=webp&s=07d5c20f5f947484d8f6edfd030c5ad17ddfe5d4")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Carlos",
@@ -215,10 +137,11 @@ animal = Animal.new(
   age: 35,
   price: 100,
   address: '115 Rue Saint-Antoine, 75004 Paris',
-  user_id: user1.id,
-  photo_url: "https://thumbs.dreamstime.com/b/sardines-9818904.jpg"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://thumbs.dreamstime.com/b/sardines-9818904.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Seydou",
@@ -227,11 +150,11 @@ animal = Animal.new(
   age: 6,
   price: 35,
   address: '98 Rue Vitruve, 75020 Paris',
-  user_id: user1.id,
-  photo_url: "https://www.jardiland.com/wp-content/uploads/2017/12/nourrir-carpes-koi-1.jpg"
+  user: User.all.sample
 )
-animal.save!
-
+  file = URI.open("https://www.jardiland.com/wp-content/uploads/2017/12/nourrir-carpes-koi-1.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Youssef",
@@ -240,10 +163,11 @@ animal = Animal.new(
   age: 14,
   price: 22,
   address: '17 Rue Christiani, 75018 Paris',
-  user_id: user1.id,
-  photo_url: "https://a-z-animals.com/media/2021/12/Beautiful-Green-Animals-Red-Eyed-Tree-Frog.jpg"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://a-z-animals.com/media/2021/12/Beautiful-Green-Animals-Red-Eyed-Tree-Frog.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Lucas",
@@ -252,10 +176,11 @@ animal = Animal.new(
   age: 19,
   price: 20,
   address: '23 Rue Frederic Sauton, 75005 Paris',
-  user_id: user1.id,
-  photo_url: "http://pyrros.fr/wp-content/uploads/2017/03/IMG_9412-Salamandre-tachet%C3%A9e.jpg"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("http://pyrros.fr/wp-content/uploads/2017/03/IMG_9412-Salamandre-tachet%C3%A9e.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Maya",
@@ -264,10 +189,11 @@ animal = Animal.new(
   age: 11,
   price: 33,
   address: '51 Rue Guy Moquet, 75017 Paris',
-  user_id: user1.id,
-  photo_url: "https://images.unsplash.com/photo-1568526381923-caf3fd520382?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://images.unsplash.com/photo-1568526381923-caf3fd520382?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Alena",
@@ -276,10 +202,11 @@ animal = Animal.new(
   age: 76,
   price: 21,
   address: '19 Rue Augereau, 75007 Paris',
-  user_id: user1.id,
-  photo_url: "https://en.bcdn.biz/Images/2016/12/1/47d501a8-93d2-4560-91a2-6f22386bf9c4.jpg"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://en.bcdn.biz/Images/2016/12/1/47d501a8-93d2-4560-91a2-6f22386bf9c4.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Marek",
@@ -288,10 +215,11 @@ animal = Animal.new(
   age: 76,
   price: 220,
   address: '141 Rue Blomet, 75015 Paris',
-  user_id: user1.id,
-  photo_url: "https://4.bp.blogspot.com/-lzuGKv1gQKs/Vgz1stkZvOI/AAAAAAAAAms/7tClxaa__Gg/s1600/Dragons%2Bare%2BReal.%2BFlying%2BDragon.png"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://4.bp.blogspot.com/-lzuGKv1gQKs/Vgz1stkZvOI/AAAAAAAAAms/7tClxaa__Gg/s1600/Dragons%2Bare%2BReal.%2BFlying%2BDragon.png")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
 
 animal = Animal.new(
   name: "Didier",
@@ -300,7 +228,8 @@ animal = Animal.new(
   age: 76,
   price: 220,
   address: '3 Rue Chaligny, 75012 Paris',
-  user_id: user1.id,
-  photo_url: "https://top10animal.com/wp-content/uploads/2021/07/unicorn3.jpg"
+  user: User.all.sample
 )
-animal.save!
+  file = URI.open("https://top10animal.com/wp-content/uploads/2021/07/unicorn3.jpg")
+  animal.photo.attach(io: file, filename: "#{animal.name} photo")
+  animal.save!
